7f4c8f27f4615005af4a9faccaa939b42606ac77
PeterMaltzoff <peter.maltzoff@gmail.com>
2023-02-26 09:18:48 +0000

    Started thinking about the go rules

--- src/board.hpp
+++ src/board.hpp
@@ -1,15 +1,19 @@
 #include <stdlib.h>
 #include <ncurses.h>
 
-enum class Piece {
-  None, Normal
+enum class PieceType {
+  None, White, Black 
+};
+
+struct Piece {
+  
 };
 
 class Board {
  private:
      int _width;
      int _height;
-     Piece* _board;
+     PieceType* _board;
      char* _board_as_string;
 
      int _cursor_x;
@@ -21,7 +25,7 @@ class Board {
  public:
      void draw_board();
      void debug();
-     void set_piece(int x, int y, Piece p);
+     void set_piece(int x, int y, PieceType p);
      void step_board_state();
 
      int get_width();
@@ -34,14 +38,19 @@ Board::Board(int width, int height) {
 
   int size = _width * _height;
 
-  _board = (Piece*)malloc(size * sizeof(Piece));
+  _board = (PieceType*)malloc(size * sizeof(PieceType));
   for (int i = 0; i < size; i++)
   {
-    _board[i] = Piece::None;
+    _board[i] = PieceType::None;
   }
 
-  // allocate _width more memory than board size to store each new line char
-  _board_as_string = (char*)malloc(size * sizeof(Piece) + _width);
+  // allocate +1 for each row to store each new line char
+  _board_as_string = (char*)malloc(size * sizeof(PieceType) + _height);
+}
+
+Board::~Board() {
+  free(_board);
+  free(_board_as_string);
 }
 
 int Board::get_width() {
@@ -53,18 +62,18 @@ int Board::get_height() {
 }
 
 void Board::draw_board() {
-  for (int y = 0; y < _height; y++)
-  {
-    for (int x = 0; x < _width; x++)
-    {
-      Piece p = _board[y*_width+x];
-      switch (p)
-      {
-        case Piece::None:
+  for (int y = 0; y < _height; y++) {
+    for (int x = 0; x < _width; x++) {
+      PieceType p = _board[y*_width+x];
+      switch (p) {
+        case PieceType::None:
           _board_as_string[y * (_width+1) + x] = '.';
           break;
-        case Piece::Normal:
+        case PieceType::White:
           _board_as_string[y * (_width+1) + x] = 'o';
+          break;
+        case PieceType::Black:
+          _board_as_string[y * (_width+1) + x] = 'x';
       }
     }
     // If we are at the end of a row, insert a new line
@@ -79,16 +88,16 @@ void Board::draw_board() {
   attroff(COLOR_PAIR(1));
 }
 
-void Board::set_piece(int x, int y, Piece p) {
+void Board::set_piece(int x, int y, PieceType p) {
   if (x < _width && x >= 0  && y < _height && y >= 0)
     _board[y * _width + x] = p;
 }
 
-void Board::step_board_state() {
-  
-}
+// RULES RULE!!! 8D
 
-Board::~Board() {
-  free(_board);
-  free(_board_as_string);
-}
+// Rule 1. The rule of liberty (god bless). Every stone remaining on the board after a turn must have at least one open point, or be part of a group that has at least one open point.
+//
+// In order to implement this I need a way to know if a piece is part of a group.
+// It is pretty easy to calculate, I just need a place to store the group indexes.
+
+// Rule 2. The ko rule. The stones on the board must never repeat a previous position of stones. Moves which do so are forbidden.
